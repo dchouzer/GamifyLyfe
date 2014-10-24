7f1356ba04449698c2c11bea6c004fd55cc05622
Leeviana Gray <lpg6@duke.edu>
2014-10-24 06:15:06 +0000

    Changes so that instance can run on virtualbox vagrant instance

--- README.md
+++ README.md
@@ -2,3 +2,21 @@ gamify-lyfe
 ===========
 
 CS316 - An achievement-based approach to life.
+
+Setting up for local (vagrant) development
+==========================================
+
+1) Follow the instructions here for setting up Django on your VM:
+http://sites.duke.edu/compsci316_01_f2014/help/django/
+
+(The admin password and specifics that you use don't matter because your local "sample module" is not the one we're using)
+
+2) Add your vagrant ssh key found at ~/.ssh/id_rsa.pub to github:
+https://help.github.com/articles/generating-ssh-keys/
+https://github.com/settings/ssh
+
+3) Clone the git repository (in your shared folder would probably be best for really local development)
+git@github.com:leeviana/gamify-lyfe.git 
+	This will make a new gamify-lyfe folder
+
+4) 
--- mysite/settings.py
+++ mysite/settings.py
@@ -63,9 +63,9 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': PROJ_NAME,                                  # Or path to database file if using sqlite3.
-        'USER': 'ubuntu',                # Not used with sqlite3.
+        'USER': 'vagrant',                # Not used with sqlite3.
         'PASSWORD': 'dbpasswd',          # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
